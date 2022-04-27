f27e91fd3be883b73f153de254becfdf8952563b
Kee Jefferys <kee@loki.network>
2022-04-28 01:46:02 +1000

    Remove boilerplate

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,21 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/><div class="dropdown">
-    <button
-      class="btn btn-secondary dropdown-toggle"
-      type="button"
-      id="dropdownMenuButton1"
-      data-bs-toggle="dropdown"
-      aria-expanded="false"
-    >
-      Check Bootstrap
-    </button>
-    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-      <li><a class="dropdown-item" href="#">Action</a></li>
-      <li><a class="dropdown-item" href="#">Another action</a></li>
-      <li><a class="dropdown-item" href="#">Something else here</a></li>
-    </ul>
-  </div>  
+ <hello-world></hello-world>
 </template>
 
 <script>
